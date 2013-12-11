67c2b253cc3421214cc044c6753c7bcd1e0137f1
Elías Alonso <eliasagc@gmail.com>
2013-12-11 23:50:03 +0100

    fix

--- src/android/PingPlugin.java
+++ src/android/PingPlugin.java
@@ -10,10 +10,10 @@ public class PingPlugin extends CordovaPlugin {
   @Override
   public boolen execute(String action, JSONArray args, CallbackContext callbackContext) {
     if ("ping".equals(action)) {
-      CallbackContext.success("pong!");
+      callbackContext.success("pong!");
       return true;
     }
-    CallbackContext.error("oops!");
+    callbackContext.error("oops!");
     return false;
   }
 };
